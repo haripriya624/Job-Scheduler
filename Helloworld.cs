7366c470a1f734fcf2e1f1c7bca055aa2e1393ff
Haripriya <110587904+haripriya624@users.noreply.github.com>
2025-06-05 20:39:46 +0530

    Updated Helloworld.cs

--- Helloworld.cs
+++ Helloworld.cs
@@ -1,15 +1,7 @@
-using Quartz;
-using System;
-using System.IO;
-using System.Threading.Tasks;
-
-public class HelloWorldJob : IJob
+public class HelloWorldJob
 {
-    public Task Execute(IJobExecutionContext context)
+    public void Run()
     {
-        string message = $"[{DateTime.Now}] Hello World from Job ID: {context.JobDetail.Key}\n";
-        Console.WriteLine(message);
-        File.AppendAllText("job_log.txt", message); // Log to a file
-        return Task.CompletedTask;
+        Console.WriteLine($"Hello World!");
     }
 }
